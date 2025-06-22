772b3693c9bb4ae9aa956e47f7fa440c789e43c3
Youngpuffy <levimuturi2@gmail.com>
2025-06-22 09:24:45 +0300

    finished assignment

--- server/app.py
+++ server/app.py
@@ -40,16 +40,36 @@ class Plants(Resource):
 
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock =data['is_in_stock']
 
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
